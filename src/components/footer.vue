bb2da1a46ffcaa86cd06ac21696d9a33d76f9911
victoria_newsite <viktoriya@newsite.by>
2019-08-14 17:35:03 +0300

    Added header and footer as components

---
+++ src/components/footer.vue
@@ -0,0 +1,39 @@
+<template>
+   <footer class="footer">
+        <div class="container">
+            <div class="columns">
+                <div class="column is-two-thirds footer-text">
+                    EntFriend © 2019
+                </div>
+                <div class="column has-text-centered footer-text">
+                    <a href="#">Конфиденциальность</a>
+                </div>
+                <div class="column has-text-centered footer-text">
+                    <a href="#">Условия обслуживания</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+<style scoped>
+   .footer {
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        padding: 0;
+        height: 120px;
+        display: flex;
+        align-items: center;
+        background-color: whitesmoke;
+    }
+    .footer-text,
+    .footer-text a {
+        font-family: "Overpass", sans-serif;
+        font-style: normal;
+        font-weight: normal;
+        font-size: 16px;
+        line-height: 25px;
+        color: rgba(0, 0, 0, 0.5);
+        text-align: left;
+    }
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <home-header></home-header>
+    1
+    <home-footer></home-footer>
+  </div>
+</template>
+
+<script>
+import Header from './header'
+import Footer from './footer'
+export default {
+  name: 'home',
+  components:{
+    'home-header': Header,
+    'home-footer': Footer
+  },
+  data () {
+    return {
+      groceryList: [
+        { id: 1, text: 'Овощи' },
+        { id: 2, text: 'Сыр' },
+        { id: 3, text: 'Что там ещё люди едят?' }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.column-wrap {
+        width: 395px;
+    }
+    .column {
+        display: flex;
+        justify-content: center;
+    }
+    .wrapper-item {
+        display: flex;
+        justify-content: center;
+        margin-bottom: 10px;
+    }
+    .wrapper-item img {
+        margin-left: 11px;
+    }
+    .wrapper-column-title {
+        text-align: center;
+    }
+    .wrapper-column-title p {
+        font-family: "Roboto", sans-serif;
+        font-weight: normal;
+        font-size: 14px;
+        line-height: 105.47%;
+        color: rgba(0, 0, 0, 0.7);
+    }
+    .column-title {
+        font-family: "Roboto", sans-serif;
+        font-weight: normal;
+        font-size: 16px;
+        line-height: 19px;
+        color: #000000;
+    }
+    table {
+        width: 395px;
+    }
+    tr {
+        width: 100%;
+    }
+    td {
+        padding: 5em; /* Поля вокруг содержимого таблицы */
+        border: 1px solid rgba(0, 0, 0, 0.35);
+        text-align: center !important;
+    }
+    .footer-text {
+        justify-content: flex-start;
+    }
+    .column-table{
+        padding: 0;
+    }
+    .button-add img{
+        padding: 1em;
+    }
+</style>
