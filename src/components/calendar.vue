1c4fb826dbdfe3a095fc5d984a4c76e5b0b97de2
Vladimir <vladimir.milanovich@gmail.com>
2019-08-24 19:20:22 +0300

    fix shadow

--- src/components/calendar.vue
+++ src/components/calendar.vue
@@ -190,7 +190,7 @@ export default {
 }
 .today {
   background: #6e8cfb;
-  box-shadow: 0.1rem 0.1rem 1rem rgb(0, 0, 0, 0.25);
+  box-shadow: 0.1rem 0.1rem 0.25rem rgba(0, 0, 0, 0.25);
   color: #fff;
 }
 .today:hover {
